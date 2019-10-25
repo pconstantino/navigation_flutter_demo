5360cd524e182de7e05ec67703c2dbaa2284e800
Pedro Constantino <pedro.constantino@ctw.bmwgroup.com>
2019-10-25 15:04:44 +0100

    navigation demo

--- lib/screen1.dart
+++ lib/screen1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:navigation_demo_starter/screen2.dart';
+import 'screen2.dart';
 class Screen1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,7 +13,16 @@ class Screen1 extends StatelessWidget {
         child: RaisedButton(
           color: Colors.red,
           child: Text('Go Forwards To Screen 2'),
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return Screen2();
+                },
+              ),
+            );
+          },
         ),
       ),
     );
